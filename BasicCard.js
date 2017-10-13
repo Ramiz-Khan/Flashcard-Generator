6e9c63c2461bdead685a224368be2d0ce7ac4ce1
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-10-12 22:25:25 -0500

    Basic Card done

---
+++ BasicCard.js
@@ -0,0 +1,20 @@
+function basicCard (front, back) {
+	this.front = front;
+	this.back = back;
+
+
+	this.printFrontCard = function() {
+	console.log(this.front);
+}
+
+	this.printBackCard = function() {
+	console.log(this.back);
+}
+
+}
+
+var first = new basicCard("Who was the first president of the united states?","George Washington");
+
+first.printFrontCard();
+
+first.printBackCard();
